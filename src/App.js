9e8d67a6f4a0389ef05b591526e3c71202e423f1
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-03-03 18:00:00 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -34,6 +34,26 @@ export default function App() {
           <h3>{title}</h3>
           <h4>{company}</h4>
           <p className="job-date">{dates}</p>
+          {duties.map(
+            (duty,
+            (index) => {
+              return (
+                <div key={index} className="job-desc">
+                  <svg
+                    height="25px"
+                    id="Layer_1"
+                    style="enable-background:new 0 0 512 512;"
+                    version="1.1"
+                    viewBox="0 0 512 512"
+                    width="25px"
+                  >
+                    <polygon points="160,115.4 180.7,96 352,256 180.7,416 160,396.7 310.5,256 " />
+                  </svg>
+                  <p>{duty}</p>
+                </div>
+              );
+            })
+          )}
         </div>
       </div>
     </section>
