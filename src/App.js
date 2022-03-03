fdf11bb93feb7bb02bc4bb680fe11615ba6e9a1a
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-03-03 19:30:33 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -53,38 +53,23 @@ export default function App() {
             return (
               <div key={index} className="job-desc">
                 <svg
+                  viewBox="0 0 32 32"
                   className="job-icon"
                   height="15px"
                   width="15px"
-                  id="Layer_1"
-                  style="enable-background:new 0 0 64 64;"
-                  version="1.1"
-                  viewBox="0 0 64 64"
-                  xml:space="preserve"
                   xmlns="http://www.w3.org/2000/svg"
-                  xmlns:xlink="http://www.w3.org/1999/xlink"
                 >
-                  <g>
-                    <g
-                      id="Icon-Arrow-Left"
-                      transform="translate(28.000000, 328.000000)"
-                    >
-                      <path
-                        class="st0"
-                        d="M4-272.1c-13.2,0-23.9-10.7-23.9-23.9S-9.2-319.9,4-319.9s23.9,10.7,23.9,23.9     S17.2-272.1,4-272.1L4-272.1z M4-317.3c-11.7,0-21.3,9.6-21.3,21.3s9.6,21.3,21.3,21.3s21.3-9.6,21.3-21.3S15.7-317.3,4-317.3     L4-317.3z"
-                        id="Fill-25"
-                      />
-                      <polyline
-                        class="st0"
-                        id="Fill-26"
-                        points="3.5,-282.3 1.7,-284.2 13.4,-296 1.7,-307.8 3.5,-309.7 17.2,-296 3.5,-282.3    "
-                      />
-                      <polygon
-                        class="st0"
-                        id="Fill-27"
-                        points="15.3,-294.6 -8.7,-294.6 -8.7,-297.4 15.3,-297.4    "
-                      />
-                    </g>
+                  <defs></defs>
+                  <title />
+                  <g data-name="Layer 2" id="Layer_2">
+                    <path
+                      class="cls-1"
+                      d="M16.88,15.53,7,5.66A1,1,0,0,0,5.59,7.07l9.06,9.06-8.8,8.8a1,1,0,0,0,0,1.41h0a1,1,0,0,0,1.42,0l9.61-9.61A.85.85,0,0,0,16.88,15.53Z"
+                    />
+                    <path
+                      class="cls-1"
+                      d="M26.46,15.53,16.58,5.66a1,1,0,0,0-1.41,1.41l9.06,9.06-8.8,8.8a1,1,0,0,0,0,1.41h0a1,1,0,0,0,1.41,0l9.62-9.61A.85.85,0,0,0,26.46,15.53Z"
+                    />
                   </g>
                 </svg>
 
