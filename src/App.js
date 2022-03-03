53539bee16fd83354e627908b19276cbe3b18394
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-03-03 18:13:21 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -34,26 +34,34 @@ export default function App() {
           <h3>{title}</h3>
           <h4>{company}</h4>
           <p className="job-date">{dates}</p>
-          {/* {duties.map(
-            (duty,
-            (index) => {
-              return (
-                <div key={index} className="job-desc">
-                  <svg
-                    height="25px"
-                    id="Layer_1"
-                    style="enable-background:new 0 0 512 512;"
-                    version="1.1"
-                    viewBox="0 0 512 512"
-                    width="25px"
-                  >
-                    <polygon points="160,115.4 180.7,96 352,256 180.7,416 160,396.7 310.5,256 " />
-                  </svg>
-                  <p>{duty}</p>
-                </div>
-              );
-            })
-          )} */}
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <svg
+                  className="job-icon"
+                  height="15px"
+                  width="15px"
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <defs></defs>
+                  <title />
+                  <g data-name="Layer 2" id="Layer_2">
+                    <path
+                      class="cls-1"
+                      d="M16.88,15.53,7,5.66A1,1,0,0,0,5.59,7.07l9.06,9.06-8.8,8.8a1,1,0,0,0,0,1.41h0a1,1,0,0,0,1.42,0l9.61-9.61A.85.85,0,0,0,16.88,15.53Z"
+                    />
+                    <path
+                      class="cls-1"
+                      d="M26.46,15.53,16.58,5.66a1,1,0,0,0-1.41,1.41l9.06,9.06-8.8,8.8a1,1,0,0,0,0,1.41h0a1,1,0,0,0,1.41,0l9.62-9.61A.85.85,0,0,0,26.46,15.53Z"
+                    />
+                  </g>
+                </svg>
+
+                <p>{duty}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
